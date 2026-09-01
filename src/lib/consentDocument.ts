35848705cacec3abeb070254588c072268910506
A.Aranha <aranha.com@gmail.com>
2026-09-01 03:50:18 -0300

    Corrige compatibilidade do gerador de consentimento

--- src/lib/consentDocument.ts
+++ src/lib/consentDocument.ts
@@ -6,11 +6,11 @@ type ConsentDocument = {
 };
 
 const escapeHtml = (value: string) => value
-  .replaceAll('&', '&amp;')
-  .replaceAll('<', '&lt;')
-  .replaceAll('>', '&gt;')
-  .replaceAll('"', '&quot;')
-  .replaceAll("'", '&#039;');
+  .replace(/&/g, '&amp;')
+  .replace(/</g, '&lt;')
+  .replace(/>/g, '&gt;')
+  .replace(/"/g, '&quot;')
+  .replace(/'/g, '&#039;');
 
 const formatSignedAt = (value: string | null) => {
   if (!value) return 'Não informado';
