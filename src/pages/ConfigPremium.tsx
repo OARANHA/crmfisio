a5d0cb19c6c6fc271861df3a725465d6ad77c9da
A.Aranha <aranha.com@gmail.com>
2026-09-03 16:41:13 -0300

    feat: surface storage status in admin settings

--- src/pages/ConfigPremium.tsx
+++ src/pages/ConfigPremium.tsx
@@ -1,6 +1,7 @@
 import { AssessmentTemplatesAdmin } from '../components/AssessmentTemplatesAdmin';
 import { ConsentTemplatesAdmin } from '../components/ConsentTemplatesAdmin';
 import { InfrastructureAdmin } from '../components/InfrastructureAdmin';
+import { StorageAdmin } from '../components/StorageAdmin';
 import { TeamAdmin } from '../components/TeamAdmin';
 import { Config } from './Config';
 
@@ -9,6 +10,7 @@ export function ConfigPremium() {
     <div className="space-y-6">
       <InfrastructureAdmin />
       <TeamAdmin />
+      <StorageAdmin />
       <AssessmentTemplatesAdmin />
       <ConsentTemplatesAdmin />
       <Config />
