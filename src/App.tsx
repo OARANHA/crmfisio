dad89cd0ad271be7b7fba5e7cb2fce8ac92aeb55
A.Aranha <aranha.com@gmail.com>
2026-09-03 22:03:54 -0300

    feat(nexus): expose patient PHQ-9 route

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { AgendaOperational } from './pages/AgendaOperational';
 import { RecepcaoHoje } from './pages/RecepcaoHoje';
 import { PatientsRoleAware } from './pages/PatientsRoleAware';
 import { PatientEditPage } from './pages/PatientEditPage';
+import { NexusPatientPhq9Page } from './pages/NexusPatientPhq9Page';
 import { FinanceiroOperational } from './pages/FinanceiroOperational';
 import { CrmOperational } from './pages/CrmOperational';
 import { MensagensOperational } from './pages/MensagensOperational';
@@ -32,6 +33,7 @@ export default function App() {
             <Route path="/hoje" element={<RecepcaoHoje />} />
             <Route path="/pacientes" element={<PatientsRoleAware />} />
             <Route path="/pacientes/:id/editar" element={<PatientEditPage />} />
+            <Route path="/pacientes/:id/nexus/phq9" element={<NexusPatientPhq9Page />} />
             <Route path="/pacientes/:id" element={<PatientsRoleAware />} />
             <Route path="/financeiro" element={<FinanceiroOperational />} />
             <Route path="/crm" element={<CrmOperational />} />
