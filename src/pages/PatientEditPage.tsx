09d07a7aa614f6c165794c1f8cd42a06041df30b
A.Aranha <aranha.com@gmail.com>
2026-09-06 21:32:15 -0300

    refactor: read patient edit data from patient provider

--- src/pages/PatientEditPage.tsx
+++ src/pages/PatientEditPage.tsx
@@ -1,11 +1,11 @@
 import { useParams } from 'react-router-dom';
-import { useApp } from '../lib/store';
+import { usePatients } from '../lib/patientContext';
 import { Empty } from '../lib/ui';
 import { PatientRegistryForm } from '../components/PatientRegistryForm';
 
 export function PatientEditPage() {
   const { id } = useParams();
-  const { patients } = useApp();
+  const { patients } = usePatients();
   const patient = patients.find((item) => item.id === id);
   if (!patient) return <Empty title="Paciente não encontrado" />;
   return <PatientRegistryForm patient={patient} />;
